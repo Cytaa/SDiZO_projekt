18c36d06795b38c46322df336d178ef38eda563a
Piotr <piotrstachnio@gmail.com>
2019-03-24 14:02:14 +0100

    TODO list.cpp

--- ConsoleApplication1/List.h
+++ ConsoleApplication1/List.h
@@ -1,9 +1,37 @@
 ﻿#pragma once
+#include "ListElement.h"
 
 class List
 {
 public:
-	int *first;
-	int *last;
-	int *current;
+	
+	List();
+	~List();
+	
+	ListElement *first;
+	ListElement *last;
+	ListElement *current;
+
+	void addHead(int data);
+
+	void addTail(int data);
+
+	void addMiddle(int data, int position);
+
+	void removeHead();
+
+	void removeTail();
+
+	void removeMiddle(int position);
+
+	void print();
+
+	void exsists();
+
+	
+
+
+
+
+
 };
