d30a289c1d1156f90c904529131823dd8c2ffe8f
Piotr <piotrstachnio@gmail.com>
2019-03-30 19:41:54 +0100

    elko

--- ConsoleApplication1/Heap.cpp
+++ ConsoleApplication1/Heap.cpp
@@ -21,6 +21,32 @@ Heap::~Heap()
 
 void Heap::add(int data)
 {
-	
+	int *newArray = new int[size + 1];
+
+	for(int i = 0; i < size; i++)
+	{
+		newArray[i] = array[i];
+	}
+	newArray[size] = data;
+
+	delete[] array;
+
+	array = newArray;
+	size++;
+	repair();
 }
 
+void Heap::repair()
+{
+	int helper;
+
+	for(int i = size; 0 < i; i--)
+	{
+		if(array[i - 1] < array[i])
+		{
+			helper = array[i - 1];
+			array[i - 1] = array[i];
+			array[i] = helper;
+		}
+	}
+}
